0abe6dbec661bba72eb17b60ece40d5e03d600d0
Vlad ONCIOIU <voncioiu@bitdefender.com>
2018-10-20 17:21:37 +0300

    Init project.

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+
+app.get('/', (res, req) => {
+    req.send('Hello world');
+});
+
+const server = http.Server(app);
+server.listen(4321);
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log('Client connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected')
+    });
+
+    socket.emit('hello', 'Hello, client! I\'am the server! Your\'re now connected to me!');
+});
+
